37d51efbe224de3c0e74229b27c60a1281f097cf
Phelan Lewis <phelanlewis@gmail.com>
2017-01-12 22:26:25 +0000

    it calculates.

--- arrayMapping.js
+++ arrayMapping.js
@@ -4,7 +4,13 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+var pythagorean = function(input){
+  calculated = Math.sqrt(Math.pow(input.x, 2) + Math.pow(input.y, 2));
+  return calculated;
+};
+
+var result = input.map(pythagorean);
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
